3c69f10652208868bdcc067549810c0ec7e38925
alidevs <alialatyah2@gmail.com>
2024-05-08 06:25:38 +0300

    initial setup for LLM Reranking using Ollama

---
+++ main.py
@@ -0,0 +1,68 @@
+import logging
+import sys
+
+import nest_asyncio
+from llama_index.core import QueryBundle, Settings, SimpleDirectoryReader, StorageContext, VectorStoreIndex
+from llama_index.core.indices.vector_store import VectorIndexRetriever
+from llama_index.core.postprocessor import LLMRerank, SentenceTransformerRerank
+from llama_index.core.schema import NodeWithScore
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.vector_stores.qdrant.base import qdrant_client
+
+nest_asyncio.apply()
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+
+def setup_settings(model_name: str, embedding_model: str) -> None:
+    Settings.embed_model = OllamaEmbedding(model_name=embedding_model)
+    Settings.llm = Ollama(model=model_name, request_timeout=360.0)
+    Settings.chunk_size = 512
+
+
+def print_retrieved_nodes(retrieved_nodes: list[NodeWithScore]) -> None:
+    header = "=" * 50 + f"[{retrieved_nodes[0].score}]" + "=" * 50
+    print(header)
+    print(retrieved_nodes[0].node.get_content())
+    print("=" * len(header))
+
+
+def initialize_vector_store() -> QdrantVectorStore:
+    client = qdrant_client.QdrantClient(location=":memory:")
+    return QdrantVectorStore(client=client, collection_name="llama3-paul-graham-collection")
+
+
+def main() -> None:
+    setup_settings("llama3", "nomic-embed-text")
+    documents = SimpleDirectoryReader("data").load_data(show_progress=True)
+    vector_store = initialize_vector_store()
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+
+    reranker = SentenceTransformerRerank(model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=5)
+    retriever = VectorIndexRetriever(index=index, similarity_top_k=10)
+
+    while True:
+        query = input("*** Ask me something: ")
+        if query == "exit":
+            break
+
+        retrieved_nodes = retriever.retrieve(QueryBundle(query))
+        reranked_nodes = reranker.postprocess_nodes(retrieved_nodes, QueryBundle(query))
+        print_retrieved_nodes(reranked_nodes)
+
+        query_engine = index.as_query_engine(streaming=True, node_postprocessors=[reranker])
+        streaming_response = query_engine.query(query)
+        print("_" * 50)
+        streaming_response.print_response_stream()
+        print("\n", "_" * 50)
+
+
+if __name__ == "__main__":
+    main()
